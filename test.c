764f498ddb671ca72838e7939c89f606cbf525cf
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2021-12-01 18:40:42 +0100

    testing stuff with process group IDs

--- test.c
+++ test.c
@@ -1,15 +1,28 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <string.h>
 
 extern char **environ;
 
 int	main(void)
 {
-	int i;
-	char buf[256];
+	int pid;
+	char *argv[3];
 
-	i = read(0, &buf, 21);
-	buf[i] = '\0';
-	printf("%s", buf);
+	pid = fork();
+	if (pid == 0)
+	{
+		argv[0] = strdup("/bin/sleep");
+		argv[1] = strdup("100");
+		argv[2] = NULL;
+		execve(argv[0], argv, environ);
+		write(2, "ERROR\n", 6);
+		exit(sleep(10));
+	}
+	sleep(15);
+	kill(0, SIGINT);
+	// wait(NULL);
+	// wait(NULL);
 }
