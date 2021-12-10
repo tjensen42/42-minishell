0bddce77ee6763e34335cc2609102ae36f249842
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2021-12-11 00:41:10 +0100

    ignore .vscode

--- src/exec/exec_expand_wildcard.c
+++ src/exec/exec_expand_wildcard.c
@@ -3,10 +3,21 @@
 // #include "exec.h"
 // #include "cmd.h"
 
-
 // int	exec_expand_wildcard(t_c_scmd *c_scmd)
 // {
+// 	t_list	*iter;
 
+// 	iter = c_scmd->l_argv;
+// 	while (iter)
+// 	{
+// 		if (expand_wildcard_needed(iter))
+// 		{
+// 			if (ft_strnstr())
+// 		}
+// 		while (token_content(iter)->flags & TOK_CONNECTED)
+// 			iter = iter->next;
+// 		iter = iter->next;
+// 	}
 // }
 
 // int	expand_wildcard_token(t_c_scmd *c_scmd)
@@ -26,6 +37,49 @@
 // 	closedir(dir);
 // 	return (0);
 // }
+
+// directory_matches_str(char *dir, char *str)
+// {
+// 	int	i_dir;
+// 	int	i_str;
+// 	int	l_dir;
+// 	int	l_str;
+
+// 	l_dir = ft_strlen(dir);
+// 	l_str = ft_strlen(str);
+// 	i_dir = 0;
+// 	i_str = 0;
+// 	ft_strnstr(&dir[i], &str[i])
+// }
+
+
+
+
+
+
+
+
+// bool	expand_wildcard_needed(t_list *token)
+// {
+// 	if (token_need_wildcard_expansion(token))
+// 		return (true);
+// 	while (token_content(token)->flags & TOK_CONNECTED)
+// 	{
+// 		token = token->next;
+// 		if (token_need_wildcard_expansion(token))
+// 			return (true);
+// 	}
+// 	return (false);
+// }
+
+// bool	token_need_wildcard_expansion(t_list *token)
+// {
+// 	if (!(token_content(token)->flags & (TOK_D_QUOTE | TOK_S_QUOTE)))
+// 		if (ft_strchr(token_content(token)->string, '*') == 0)
+// 			return (true);
+// 	return (false);
+// }
+
 
 // ls *
 
