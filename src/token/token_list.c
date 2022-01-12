7dec9473f450ec7922259a75ede813e4b1b8c2f1
Hannes Epple <hepple@1-A-5.42heilbronn.de>
2022-01-12 20:07:28 +0100

    Fixed norm in token_list.c

--- src/token/token_list.c
+++ src/token/token_list.c
@@ -1,62 +1,64 @@
 #include "token.h"
 
+static int	l_token_count_strings(t_list *l_token);
+static int	l_token_append_connected(t_list **l_token, char **split_str);
+
 char	**l_token_to_split(t_list *l_token)
 {
 	int		i;
-	char	*tmp;
 	char	**split;
 	t_list	*iter;
 
 	if (l_token == NULL)
 		return (NULL);
-	i = 0;
-	iter = l_token;
-	while (iter)
-	{
-		if (!(token_content(iter)->flags & TOK_CONNECTED))
-			i++;
-		iter = iter->next;
-	}
-	split = malloc((i + 1) * sizeof(char *));
+	split = malloc((l_token_count_strings(l_token) + 1) * sizeof(char *));
 	if (split == NULL)
 		return (NULL);
 	i = 0;
 	iter = l_token;
 	while (iter)
 	{
-		if (token_content(iter)->flags & TOK_CONNECTED)
+		split[i] = ft_strdup(token_content(iter)->str);
+		if (split[i] == NULL
+			|| l_token_append_connected(&iter, &split[i]) == ERROR)
 		{
-			split[i] = ft_strdup(token_content(iter)->str);
-			if (split[i] == NULL)
-			{
-				ft_free_split(&split);
-				return (NULL);
-			}
-			while (token_content(iter)->flags & TOK_CONNECTED)
-			{
-				tmp = split[i];
-				split[i] = ft_strjoin(split[i], token_content(iter->next)->str);
-				free(tmp);
-				if (split[i] == NULL)
-				{
-					ft_free_split(&split);
-					return (NULL);
-				}
-				iter = iter->next;
-			}
-		}
-		else
-		{
-			split[i] = ft_strdup(token_content(iter)->str);
-			if (split[i] == NULL)
-			{
-				ft_free_split(&split);
-				return (NULL);
-			}
+			ft_free_split(&split);
+			return (NULL);
 		}
 		iter = iter->next;
 		i++;
 	}
 	split[i] = NULL;
 	return (split);
+}
+
+static int	l_token_count_strings(t_list *l_token)
+{
+	int	i;
+
+	i = 0;
+	while (l_token)
+	{
+		if (!(token_content(l_token)->flags & TOK_CONNECTED))
+			i++;
+		l_token = l_token->next;
+	}
+	return (i);
+}
+
+static int	l_token_append_connected(t_list **l_token, char **split_str)
+{
+	char	*tmp;
+
+	while (token_content(*l_token)->flags & TOK_CONNECTED)
+	{
+		tmp = *split_str;
+		*split_str = ft_strjoin(*split_str,
+				token_content((*l_token)->next)->str);
+		free(tmp);
+		if (*split_str == NULL)
+			return (ERROR);
+		*l_token = (*l_token)->next;
+	}
+	return (0);
 }
