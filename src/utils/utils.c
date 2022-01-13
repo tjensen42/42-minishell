2b9ae529e047dee96908687b887c0fd8151de3d0
Hannes Epple <hepple@1-A-5.42heilbronn.de>
2022-01-13 10:36:54 +0100

    Simple get_next_line with buffer size 1 added.

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -38,3 +38,51 @@ bool	is_var_char(char c)
 {
 	return (ft_isalnum(c) || c == '_');
 }
+
+char	*append_char(char *str, char c)
+{
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	new_str = malloc(sizeof(char) * (i + 2));
+	if (new_str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = c;
+	new_str[i + 1] = '\0';
+	free(str);
+	return (new_str);
+}
+
+int	get_next_line(char **line, int fd)
+{
+	int		check;
+	char	buffer;
+
+	if (line == NULL)
+		return (-1);
+	*line = malloc(sizeof(char) * 1);
+	if (*line == NULL)
+		return (-1);
+	*line[0] = '\0';
+	while ((check = read(fd, &buffer, 1)) > 0)
+	{
+		if (buffer == '\n')
+			break ;
+		*line = append_char(*line, buffer);
+		if (*line == NULL)
+			return (-1);
+	}
+	return (check);
+}
