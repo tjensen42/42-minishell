4b54781db15ff22565289fa641862968c32025b8
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2022-01-30 10:31:32 +0100

    added go_wild test_cmd

--- tests/cmds/bonus/9_go_wild.sh
+++ tests/cmds/bonus/9_go_wild.sh
@@ -2,6 +2,9 @@
 #   WILD                                                                       #
 # **************************************************************************** #
 
+echo 42 < in | wc -l && ( /bin/echo 1 2 3 4 > tmp_out || /bin/echo 2 > tmp_out >> tmp_out tmp_out )
+rm -f tmp_out
+
 grep 42 < in | wc -l && ( /bin/echo 1 2 3 4 > tmp_out || /bin/echo 2 > tmp_out >> tmp_out tmp_out )
 rm -f tmp_out
 
